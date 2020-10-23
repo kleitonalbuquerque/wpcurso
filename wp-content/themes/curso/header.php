9b4b2e1b9854104c864cb784104663a9110a6080
kleitonalbuquerque <kleiton2102@gmail.com>
2020-10-23 10:04:09 -0300

    Modularização dos componentes header e footer do site wp

---
+++ wp-content/themes/curso/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso WordPress</title>
+</head>
+
+<body <?php body_class(); ?>>
+    <p>Parágrafo de referência.</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo">
+                <nav class="menu"></nav>
+            </section>
+        </section>
+    </header>

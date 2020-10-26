2f37294a8a9f3d073f5137f2752e3dc2593995b5
kleitonalbuquerque <kleiton2102@gmail.com>
2020-10-26 16:28:25 -0300

    Entendendo Template Parts

---
+++ wp-content/themes/curso/template-parts/content.php
@@ -0,0 +1,13 @@
+<article>
+    <h2><?php the_title(); ?></h2>
+    <?php the_post_thumbnail(array(275, 275)); ?>
+    <//?php the_post_thumbnail('thumbnail'); ?>
+    <p>Published in by
+        <?php echo get_the_date(); ?>
+        <br> Author: <?php the_author_posts_link() ?>
+    </p>
+    <p>Categories: <?php the_category(' '); ?></p>
+    <p><?php the_tags('Tags: ', ', '); ?></p>
+    <?php the_content(); ?>
+    <hr>
+</article>

37cfac0c7cdccda21554ba2fa723223854191f8b
kleitonalbuquerque <kleiton2102@gmail.com>
2020-10-27 15:38:52 -0300

    Criando uma área de serviços como um área de widgets

--- wp-content/themes/curso/page-home.php
+++ wp-content/themes/curso/page-home.php
@@ -10,7 +10,36 @@
 
         <section class="services">
             <div class="container">
-                <div class="row">Serviços</div>
+                <h1>Our Services</h1>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="sevices-item">
+                            <?php 
+                            if(is_active_sidebar('services-1')) {
+                                dynamic_sidebar('services-1');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="sevices-item">
+                        <?php 
+                            if(is_active_sidebar('services-2')) {
+                                dynamic_sidebar('services-2');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="sevices-item">
+                        <?php 
+                            if(is_active_sidebar('services-3')) {
+                                dynamic_sidebar('services-3');
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
 

2ce163a2c7b5f63d58153b2b7d83ebf511a28ac6
kleitonalbuquerque <kleiton2102@gmail.com>
2020-11-04 12:25:16 -0300

    Utilziando a clas WP_Query() para realziar consultas ao banco de dados e modificar o loop padrão do wp

--- wp-content/themes/curso/page-home.php
+++ wp-content/themes/curso/page-home.php
@@ -1,80 +1,107 @@
 <?php get_header(); ?>
+	<div class="content-area">
+		<main>
+			<section class="slide">
+				<div class="container">
+					<div class="row">Slide</div>
+				</div>
+			</section>
+			<section class="services">
+				<div class="container">
+					<h1>Our Services</h1>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-1' )){
+									dynamic_sidebar( 'services-1' );
+								}
 
-<div class="content-area">
-    <main>
-        <section class="slide">
-            <div class="container">
-                <div class="row">Slide</div>
-            </div>
-        </section>
+								?>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-2' )){
+									dynamic_sidebar( 'services-2' );
+								}
 
-        <section class="services">
-            <div class="container">
-                <h1>Our Services</h1>
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="sevices-item">
-                            <?php 
-                            if(is_active_sidebar('services-1')) {
-                                dynamic_sidebar('services-1');
-                            }
-                            ?>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="sevices-item">
-                        <?php 
-                            if(is_active_sidebar('services-2')) {
-                                dynamic_sidebar('services-2');
-                            }
-                            ?>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="sevices-item">
-                        <?php 
-                            if(is_active_sidebar('services-3')) {
-                                dynamic_sidebar('services-3');
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+								?>								
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="services-item">
+								<?php 
+								if( is_active_sidebar( 'services-3' )){
+									dynamic_sidebar( 'services-3' );
+								}
 
-        <section class="middle-area">
-            <div class="container">
-                <div class="row">
-                    <?php get_sidebar('home'); ?>
-                    <div class="news col-md-8">
-                        <!-- News -->
-                        <?php 
-                        if( have_posts() ):
-                            while( have_posts() ): the_post();
-                        ?>
+								?>								
+							</div>
+						</div>
+					</div>
+				</div>				
+			</section>
+			<section class="middle-area">
+				<div class="container">
+					<div class="row">
+						<?php get_sidebar( 'home' ); ?>
+						<div class="news col-md-8">
+							<div class="container">
+								<h1>Latest News</h1>
+								<div class="row">
+									<?php 
+									// Primeiro Loop - Notícia Destaque
+									$featured = new WP_Query( 'post_type=post&posts_per_page=1&cat=3,5,6' );
 
-                        <p>Conteúdo vindo do arquivo home.php</p>
+									if( $featured->have_posts() ):
+										while( $featured->have_posts() ): $featured->the_post();
+									?>
 
-                        <?php 
-                            endwhile;
-                        else: 
-                        ?>
-                        <p>There is nothing yet to be displayed...</p>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </div>
-        </section>
+									<div class="col-12">
+										<?php get_template_part( 'template-parts/content', 'featured' ); ?>
+									</div>
 
-        <section class="map">
-            <div class="container">
-                <div class="row">
-                    Mapa
-                </div>
-            </div>
-        </section>
-    </main>
-</div>
+									<?php
+										endwhile;
+										wp_reset_postdata();
+									endif;
 
+									// Segundo Loop
+									$args = array(
+										'post_type' => 'post',
+										'posts_per_page' => 2,
+										'category__not_in' => array( 4 ),
+										'category__in' => array( 3, 5 ),
+										'offset' => 1
+									);
+									$secondary = new WP_Query( $args );
+
+									if( $secondary->have_posts() ):
+										while( $secondary->have_posts() ): $secondary->the_post();
+									?>
+
+									<div class="col-sm-6">
+										<?php get_template_part( 'template-parts/content', 'secondary' ); ?>
+									</div>
+
+									<?php
+										endwhile;
+										wp_reset_postdata();
+									endif;
+									?>
+								</div>
+							</div>
+						</div>						
+					</div>
+				</div>				
+			</section>
+			<section class="map">
+				<div class="container">
+					<div class="row">Mapa</div>
+				</div>				
+			</section>
+		</main>
+	</div>
 <?php get_footer(); ?>

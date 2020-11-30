2ba5766c95fc571396b0fe830bd185712b538b7c
kleitonalbuquerque <kleiton2102@gmail.com>
2020-11-30 18:03:13 -0300

    Theme customizer and address

--- wp-content/themes/curso/inc/customizer.php
+++ wp-content/themes/curso/inc/customizer.php
@@ -1,9 +1,7 @@
 <?php 
 
 function wpcurso_customizer( $wp_customize ){
-
 	// Copyright
-
 	$wp_customize->add_section( 
 		'sec_copyright', array(
 			'title' => 'Copyright',
@@ -28,5 +26,48 @@ function wpcurso_customizer( $wp_customize ){
 		)
 	);	
 
+	// Map
+	$wp_customize->add_section( 
+		'sec_map', array(
+			'title' => 'Map',
+			'description' => 'Map Section'
+		)
+	);
+
+	// API Key
+	$wp_customize->add_setting(
+		'set_map_apikey', array(
+			'type' => 'theme_mod',
+			'default' => '',
+			'sanitize_callback' => 'wp_filter_nohtml_kses'
+		)
+	);
+
+	$wp_customize->add_control(
+		'set_map_apikey', array(
+			'label' => 'API Key',
+			'description' => 'Get your key <a target="_blank" href="https://console.developers.google.com/flows/enableapi?apiid=maps_backend">here</a>',
+			'section' => 'sec_map',
+			'type' => 'text'
+		)
+	);
+
+	// Address
+	$wp_customize->add_setting(
+		'set_map_address', array(
+			'type' => 'theme_mod',
+			'default' => '',
+			'sanitize_callback' => 'esc_textarea'
+		)
+	);
+
+	$wp_customize->add_control(
+		'set_map_address', array(
+			'label' => 'Type your address here',
+			'description' => 'No special characters allowed',
+			'section' => 'sec_map',
+			'type' => 'textarea'
+		)
+	);
 }
 add_action( 'customize_register', 'wpcurso_customizer' );
